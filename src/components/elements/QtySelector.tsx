00b25df0c4b271d0fba7a0c81b6efb48979716c4
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-05-21 19:29:55 +0100

    Quantity selector component.

--- src/components/elements/QtySelector.tsx
+++ src/components/elements/QtySelector.tsx
@@ -1,5 +1,5 @@
 
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect, ChangeEvent } from 'react'
 import styled from 'styled-components'
 import { StyleContext  } from '../../context/Style'
 import Icon_Minus from '../icons/Icon_Minus'
@@ -8,28 +8,67 @@ import Icon_Plus from '../icons/Icon_Plus'
 const QtySelectorContainer = styled.div`
 	display: flex;
 	justify-content: space-between;
+	align-items: center;
 	background-color: ${props => props.theme.lightGreyishBlue};
 	width: 100%;
 	border-radius: 10px;
 
 	.button {
 		all: unset;
+		display: grid;
+		place-items: center;
+		padding: 1rem;
+		cursor: pointer;
 		
 		svg {
 			fill: ${props => props.theme.orange};
 		}
+
+		&:hover,
+		&:focus {
+			opacity: 0.5;
+		}
+	}
+
+	.qty {
+		font-size: 1.05rem;
+		font-weight: 700;
+		color: ${props => props.theme.veryDarkBlue};
+		margin: 0.9rem;
 	}
 `
 
 export default function QtySelector() {
 	const { styles } = useContext(StyleContext)
 	const [qty, setQty] = useState<number>(0)
+	const [inputValue, setInputValue] = useState<string | number>(0)
+
+	function addOne() {
+		setQty(prevQty => prevQty + 1)
+	}
+
+	function subtractOne() {
+		if (qty > 0) setQty(prevQty => prevQty - 1)
+	}
+
+	function changeQty(event: ChangeEvent<HTMLInputElement>) {
+		if (!isNaN(parseInt(event.target.value))) {
+			setQty(parseInt(event.target.value))
+		} else {
+			setInputValue('')
+		}
+	}
+
+	useEffect(() => {
+		setInputValue(qty)
+		console.log(qty)
+	}, [qty])
 
 	return (
 		<QtySelectorContainer theme={styles}>
-			<button className="button"><Icon_Plus /></button>
-			<p className="qty">{qty}</p>
-			<button className="button"><Icon_Minus /></button>
+			<button className="button" onClick={subtractOne}><Icon_Minus /></button>
+			<input type="text" className="qty" onChange={(event) => changeQty(event)} value={inputValue} />
+			<button className="button" onClick={addOne}><Icon_Plus /></button>
 		</QtySelectorContainer>
 	)
 }
