c5ce65ff43caecd0c8442c48e4a9b5c09d29c509
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-05-21 14:11:02 +0100

    Thumbnail image component.

--- src/components/content/ProductImage.tsx
+++ src/components/content/ProductImage.tsx
@@ -4,18 +4,25 @@ import styled from 'styled-components'
 import { StyleContext } from '../../context/Style'
 import NextButton from '../elements/NextButton'
 import PreviousButton from '../elements/PreviousButton'
+import Thumbnail from '../elements/Thumbnail'
 
 const ProductImageContainer = styled.section`
 	position: relative;
-	background-color: ${props => props.theme.orange};
 	height: 300px;
 	width: 100%;
 	overflow: hidden;
-
+	
 	.image {
+		background-color: ${props => props.theme.orange};
 		width: 100%;
 		height: 100%;
 		object-fit: cover;
+
+		@media (min-width: ${props => props.theme.breakpoint}) {
+			width: 445px;
+			height: 445px;
+			border-radius: 15px;
+		}
 	}
 
 	.buttons {
@@ -25,6 +32,29 @@ const ProductImageContainer = styled.section`
 		align-items: center;
 		justify-content: space-between;
 		padding: 1rem;
+
+		@media (min-width: ${props => props.theme.breakpoint}) {
+			display: none;
+		}
+	}
+
+	.thumbnails {
+		display: none;
+
+		@media (min-width: ${props => props.theme.breakpoint}) {
+			display: flex;
+			justify-content: space-between;
+			width: 445px;
+			margin-top: 35px;
+		}
+	}
+
+	@media (min-width: ${props => props.theme.breakpoint}) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-top: 100px;
+		height: 100%;
 	}
 `
 
@@ -38,6 +68,12 @@ export default function ProductImage() {
 			<div className="buttons">
 				<PreviousButton action={() => console.log('Previous image...')} />
 				<NextButton action={() => console.log('Next image...')} />
+			</div>
+			<div className="thumbnails">
+				<Thumbnail imageUrl="./images/product-1-thumbnail.jpg" altText="thumbnail 1" selected />
+				<Thumbnail imageUrl="./images/product-2-thumbnail.jpg" altText="thumbnail 2" />
+				<Thumbnail imageUrl="./images/product-3-thumbnail.jpg" altText="thumbnail 3" />
+				<Thumbnail imageUrl="./images/product-4-thumbnail.jpg" altText="thumbnail 4" />
 			</div>
 		</ProductImageContainer>
 	)
---
+++ src/components/elements/Thumbnail.tsx
@@ -0,0 +1,59 @@
+
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { StyleContext } from '../../context/Style'
+
+interface thumbnail {
+	imageUrl: string
+	altText: string
+	selected?: boolean
+}
+
+const ThumbnailContainer = styled.button`
+	all: unset;
+	border-radius: 10px;
+	overflow: hidden;
+	cursor: pointer;
+	position: relative;
+
+	.thumbnailImage {
+		display: block;
+	}
+
+	&.selected {
+		.thumbnailImage {
+			opacity: 0.25;
+		}
+
+		.border {
+			display: block;
+			border: 2px solid ${props => props.theme.orange};
+		}
+	}
+	
+	.border {
+		display: none;
+		position: absolute;
+		inset: 0;
+		border-radius: 10px;
+	}
+
+	&:hover,
+	&:focus {
+		.border {
+			display: block;
+			border: 2px solid ${props => props.theme.veryDarkBlue};
+		}
+	}
+`
+
+export default function Thumbnail({ imageUrl, altText, selected = false }: thumbnail) {
+	const { styles } = useContext(StyleContext)
+
+	return (
+		<ThumbnailContainer className={selected && 'selected'} theme={styles}>
+			<img className="thumbnailImage" src={imageUrl} alt={altText} width="88" height="88" />
+			<div className="border"></div>
+		</ThumbnailContainer>
+	)
+}
